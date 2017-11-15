c6d024a4c39256ed96295198658ea672de2a875f
Kevin Schaffter <kevinschaffter@gmail.com>
2017-11-15 07:38:14 -0500

    weather-scrubber-2.0

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+require 'HTTParty'
+
+class PagesController < ApplicationController
+  def home
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Miami+Beach,FL&appid=c38d29204353577f04f9a8b861e3c70e"
+    response = HTTParty.get(url)
+    @weather = JSON.parse(response.body)
+    @farenheit = ((@weather["main"]["temp"] * 9) / 5 - 459.67).round(2)
+  end
+end
